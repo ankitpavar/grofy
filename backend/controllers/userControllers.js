e616c04349785adc9ca0f38a8a8e539c40f30271
Ankit Pavar <geekyanky@gmail.com>
2020-10-13 12:58:43 +0530

    created update user profile route

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -51,9 +51,9 @@ const registerUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
     });
-  } else{
-      res.status(400)
-      throw new Error('Invalid User Data');
+  } else {
+    res.status(400);
+    throw new Error('Invalid User Data');
   }
 });
 
@@ -75,4 +75,31 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
-module.exports = { authUser,registerUser, getUserProfile };
+
+// @desc  Update user profile
+// @route  PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+module.exports = { authUser, registerUser, getUserProfile, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { authUser,registerUser, getUserProfile } = require('../controllers/userControllers');
-const {protect} = require('../middleware/authMiddleware')
+const {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} = require('../controllers/userControllers');
+const { protect } = require('../middleware/authMiddleware');
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 module.exports = router;
