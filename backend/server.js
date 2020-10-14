f828411c9cadd7358f39ccb5bfd1799173b32fd0
Ankit Pavar <geekyanky@gmail.com>
2020-10-15 00:24:59 +0530

    some bug fixes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan')
+const path = require('path');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const connectDb = require('./config/db');
 
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
 
 dotenv.config();
 
@@ -19,18 +21,32 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+
 
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes);
 
+
+// const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 // Middlewares
 app.use(notFound);
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
---
+++ frontend/build/precache-manifest.31a391de61cd5409bcbff746dfa01253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b384fad05758102d6066a5a2f6b9d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96152ac6e2f69b84046d",
+    "url": "/static/css/main.8314c3f8.chunk.css"
+  },
+  {
+    "revision": "b96e768220b02096cbd0",
+    "url": "/static/js/2.7053e989.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7053e989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96152ac6e2f69b84046d",
+    "url": "/static/js/main.3a0ac7b6.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
