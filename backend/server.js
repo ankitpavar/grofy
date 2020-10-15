7ff0297563bb6fa79ed15d8a1b68e5e58daf7ea5
Ankit Pavar <geekyanky@gmail.com>
2020-10-15 15:45:54 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,58 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan')
+const morgan = require('morgan');
 const path = require('path');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
-const connectDb = require('./config/db');
+const connectDB = require('./config/db');
 
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
 
-dotenv.config();
+dotenv.config()
 
-// MongoDB Connection
-connectDb();
+connectDB()
 
-// Express app initialization
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
+app.use(express.json())
 
-
-// Routes
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes);
+app.use('/api/upload', uploadRoutes)
 
 
-// const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-// Middlewares
-app.use(notFound);
-app.use(errorHandler);
+
+
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
+  )
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
+    res.send('API is running....')
+  })
 }
 
-const PORT = process.env.PORT || 5000;
+app.use(notFound)
+app.use(errorHandler)
 
-// Server start
+const PORT = process.env.PORT || 5000
+
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
-);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+)
